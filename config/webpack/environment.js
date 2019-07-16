aa44e1dd1f0cfde417a58fc50fb0f1175ef85955
ulverson <marcin.walczak@gmail.com>
2019-07-16 21:28:12 +0100

    Partial views

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,14 @@
 const { environment } = require('@rails/webpacker')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const webpack = require('webpack')
+environment.loaders.insert('sass', {
+    test: /\.scss|\.sass$/,
+    use: [
+      "to-string-loader", 
+      "css-loader", 
+      "sass-loader" 
+    ]
+});
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
